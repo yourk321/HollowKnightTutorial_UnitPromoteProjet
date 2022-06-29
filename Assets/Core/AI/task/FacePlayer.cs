96cbff3c29a618b76f05ef748a76293612bfb161
york <liyang@liyangdeMacBook-Pro.local>
2022-06-29 16:29:37 +0800

    添加action，boos跳跃以及面向玩家

---
+++ Assets/Core/AI/task/FacePlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using BehaviorDesigner.Runtime.Tasks;
+
+namespace Core.AI{
+
+    public class FacePlayer : EnemyAction
+    { 
+        private float baseScaleX;
+        public override void OnStart()
+        {
+            base.OnAwake();
+            baseScaleX = Mathf.Abs(transform.localScale.x);
+      
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            var scale = transform.localScale;
+            scale.x = transform.position.x > player.transform.position.x ? -baseScaleX : baseScaleX;
+            Debug.LogError(scale.x);
+            transform.localScale = scale;
+            return TaskStatus.Success;
+        }
+    }
+
+}
---
+++ Assets/Core/AI/task/Jump.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using BehaviorDesigner.Runtime.Tasks;
+using DG.Tweening;
+using Core.Character;
+
+namespace Core.AI
+{
+    public class Jump : EnemyAction
+    {
+
+        public float horForce = 5;
+        public float jumpForce = 10;
+
+        public float buildupJump;
+        public float jumpTime;
+
+        public string animationTriggerName;
+        public bool shakeCameraOnLanding;
+
+        private bool isLanded = false;
+
+        private Tween jumpTween;
+        private Tween buildupTween;
+
+        // Use this for initialization
+        public override void OnStart()
+        {
+            buildupTween = DOVirtual.DelayedCall(buildupJump, startJump);
+            animator.SetTrigger(animationTriggerName);
+        }
+
+        public void startJump()
+        {
+            float direction = player.transform.position.x < transform.position.x ? -1 : 1;
+            body.AddForce(new Vector2(horForce * direction, jumpForce), ForceMode2D.Impulse);
+            jumpTween = DOVirtual.DelayedCall(jumpTime, () =>
+            {
+                isLanded = true;
+                if (shakeCameraOnLanding)
+                    CameraController.Instance.ShakeCamera(0.2f);
+            });
+        }
+
+        // Update is called once per frame
+        public override TaskStatus OnUpdate()
+        {
+            if (isLanded) return TaskStatus.Success;
+            return TaskStatus.Running;
+        }
+
+        public override void OnEnd()
+        {
+            buildupTween.Kill();
+            jumpTween.Kill();
+            isLanded = false;
+        }
+    }
+}
+
