3bba3321732276a32b2883954fc6c2aa5cb91c03
york <liyang@liyangdeMacBook-Pro.local>
2022-07-01 17:26:02 +0800

    行为树 添加循环攻击动画，攻击特效

---
+++ Assets/Core/AI/task/Shoot.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core.Combat;
+using Core.Character;
+using BehaviorDesigner.Runtime.Tasks;
+
+namespace Core.AI {
+
+    public class Shoot :EnemyAction
+    {
+        public List<Weapon> list;
+        public bool isShakeCamera;
+
+        public override TaskStatus OnUpdate()
+        {
+            //foreach (var weapon in list)
+            //{
+            //    var projectile = Object.Instantiate(weapon.projectilePrefab, weapon.weaponTransform.position, Quaternion.identity);
+            //    projectile.Shooter = this.gameObject;
+
+            //    //保证子弹和boss是一个方向
+            //    var force = new Vector2(weapon.horizontalForce * transform.localScale.x, weapon.verticalForce);
+            //    projectile.SetForce(force);
+
+            //    if (isShakeCamera)
+            //        CameraController.Instance.ShakeCamera(0.2f);
+            //} 
+            return TaskStatus.Success;
+        }
+    }
+}
--- Assets/Core/Combat/Weapon.cs
+++ Assets/Core/Combat/Weapon.cs
@@ -9,7 +9,9 @@ namespace Core.Combat
     {
         public Transform weaponTransform;
         public AbstractProjectile projectilePrefab;
+        //水平方向的力
         public float horizontalForce = 5.0f;
+        //垂直方向的力
         public float verticalForce = 4.0f;
     }
 }
