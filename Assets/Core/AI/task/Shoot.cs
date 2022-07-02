8846c1d05f28efd4a26a94233e48bf986def20b9
york <>
2022-07-02 21:27:14 +0800

    增加子弹类型：落石

--- Assets/Core/AI/task/Shoot.cs
+++ Assets/Core/AI/task/Shoot.cs
@@ -9,23 +9,28 @@ namespace Core.AI {
 
     public class Shoot :EnemyAction
     {
-        public List<Weapon> list;
+        public Weapon weapon;
+
         public bool isShakeCamera;
+
+        public override void OnStart()
+        {
+        }
 
         public override TaskStatus OnUpdate()
         {
-            //foreach (var weapon in list)
-            //{
-            //    var projectile = Object.Instantiate(weapon.projectilePrefab, weapon.weaponTransform.position, Quaternion.identity);
-            //    projectile.Shooter = this.gameObject;
+            //foreach (var weapon in weapons)
+            {
+                var projectile = Object.Instantiate(weapon.projectilePrefab, weapon.weaponTransform.position, Quaternion.identity);
+                projectile.Shooter = this.gameObject;
 
-            //    //保证子弹和boss是一个方向
-            //    var force = new Vector2(weapon.horizontalForce * transform.localScale.x, weapon.verticalForce);
-            //    projectile.SetForce(force);
+                //保证子弹和boss是一个方向
+                var force = new Vector2(weapon.horizontalForce * transform.localScale.x, weapon.verticalForce);
+                projectile.SetForce(force);
 
-            //    if (isShakeCamera)
-            //        CameraController.Instance.ShakeCamera(0.2f);
-            //} 
+                if (isShakeCamera)
+                    CameraController.Instance.ShakeCamera(0.2f);
+            }
             return TaskStatus.Success;
         }
     }
--- Assets/Core/AI/task/SpawnFallingRock.cs
+++ Assets/Core/AI/task/SpawnFallingRock.cs
@@ -1,17 +1,39 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using System.Collections;
+using Core.Combat;
+using Core.Combat.Projectiles;
+using BehaviorDesigner.Runtime.Tasks;
+using DG.Tweening;
 
-public class NewMonoBehaviour : MonoBehaviour
+namespace Core.AI
 {
-    // Use this for initialization
-    void Start()
+    public class SpawnFallingRock : EnemyAction
     {
+        public Collider2D spawnArea;
+        public AbstractProjectile rockPrefab;
+        public int spawCount = 4;
+        public float spawInterval = 0.3f;
 
+
+        public override TaskStatus OnUpdate()
+        {
+            var seq = DOTween.Sequence();
+            for (int i=0; i<spawCount; i++)
+            {
+                seq.AppendCallback(spawIns);
+                seq.AppendInterval(spawInterval);
+            }
+            
+            return TaskStatus.Success;
+        }
+
+        private void spawIns()
+        {
+            float x = Random.Range(spawnArea.bounds.min.x, spawnArea.bounds.max.x);
+            var rock = Object.Instantiate(rockPrefab, new Vector3(x, spawnArea.bounds.min.y), Quaternion.identity);
+            rock.SetForce(Vector2.zero);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
