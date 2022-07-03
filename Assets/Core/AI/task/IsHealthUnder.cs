3f2aae4756aec27aa064fd8e09f0fb5f9f01cdbf
york <>
2022-07-03 13:06:44 +0800

    添加boss血量监测条件，一旦低于阈值，则暂停

---
+++ Assets/Core/AI/task/IsHealthUnder.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using BehaviorDesigner.Runtime.Tasks;
+using BehaviorDesigner.Runtime;
+
+namespace Core.AI
+{
+    public class IsHealthUnder : EnemyConditional
+    {
+        public SharedInt healthTreshold;
+        //生命极限阈值
+        
+        public override TaskStatus OnUpdate()
+        {
+            
+            if(destructable.CurrentHealth < healthTreshold.Value)
+            {
+                return TaskStatus.Success; 
+            }
+            return TaskStatus.Failure;
+        }
+    }
+}
+
