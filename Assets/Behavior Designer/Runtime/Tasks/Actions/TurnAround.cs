9ccc9982069a85f6ef0a3c55976b566a56fe8379
york <>
2022-07-13 20:47:19 +0800

    添加行为：左右两次连贯普通攻击

---
+++ Assets/Behavior Designer/Runtime/Tasks/Actions/TurnAround.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using BehaviorDesigner.Runtime.Tasks;
+
+namespace Core.AI
+{
+    public class TurnAround : EnemyAction
+    {
+        public override TaskStatus OnUpdate()
+        {
+            var scale = this.gameObject.transform.localScale;
+            scale.x *= -1;
+            this.transform.localScale = scale;
+            return TaskStatus.Success;
+        }
+    }
+}
+
