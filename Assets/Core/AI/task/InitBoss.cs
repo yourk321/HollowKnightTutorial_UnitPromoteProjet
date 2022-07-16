d4490fe50ea7c329776405597d986f5bfe919be1
york <>
2022-07-16 22:56:47 +0800

    添加行为：boss出场

---
+++ Assets/Core/AI/task/InitBoss.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using BehaviorDesigner.Runtime.Tasks;
+using Core.UI;
+
+namespace Core.AI
+{
+    public class InitBooss : EnemyAction
+    {
+        public string bossName;
+
+        public override TaskStatus OnUpdate()
+        {
+            GuiManager.Instance.ShowBossName(bossName);
+            return TaskStatus.Success;
+        }
+    }
+}
+
