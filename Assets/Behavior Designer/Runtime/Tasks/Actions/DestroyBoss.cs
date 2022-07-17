d6665a5813bc32777334c294742bf7f066dea93e
york <>
2022-07-17 21:11:06 +0800

    添加行为：boss死亡，游戏结束

---
+++ Assets/Behavior Designer/Runtime/Tasks/Actions/DestroyBoss.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using Core.Util;
+using DG.Tweening;
+using Core.Character;
+using BehaviorDesigner.Runtime.Tasks;
+
+namespace Core.AI
+{
+    public class DestroyBoss : EnemyAction
+    {
+        public float bleedTime;
+        public ParticleSystem bleedSystem;
+        public ParticleSystem explodeSystem;
+
+        private bool isDestroy = false;
+        public override void OnStart()
+        {
+            EffectManager.Instance.PlayOneShot(bleedSystem, this.gameObject.transform.position);
+            DOVirtual.DelayedCall(bleedTime, ()=> {
+
+                EffectManager.Instance.PlayOneShot(explodeSystem, this.transform.position);
+                CameraController.Instance.ShakeCamera(0.7f);
+                GameObject.Destroy(this.gameObject);
+
+                isDestroy = true;
+            }
+            , false);
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            if (isDestroy)
+            {
+                return TaskStatus.Success;
+            }
+            return TaskStatus.Running;
+        }
+    }
+}
